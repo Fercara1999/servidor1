90de0b370326463edcb60e9a2fc3e11abaa9e95c
Fernando Calles <fercara1999@gmail.com>
2023-11-01 11:38:33 +0000

    1N

--- Tema3/PR9/index.php
+++ Tema3/PR9/index.php
@@ -13,16 +13,58 @@
         include("./validaciones.php");
     ?>
 
-    <form action="" method="post" name="formularioPR9"></form>
-    <label for="nombre">Nombre: <input type="text" name="nombre" id="nombre"></label><br>
-    <label for="apellido">Apellido: <input type="text" name="apellido" id="apellido"></label><br>
-    <label for="contrasena">Contraseña: <input type="password" name="contrasena" id="contrasena"></label><br>
-    <label for="repcontrasena">Confirma contraseña: <input type="password" name="repcontrasena" id="repcontrasena"></label><br>
-    <label for="fecha">Fecha <input type="dateTime" name="fecha" id="fecha"></label><br>
-    <label for="DNI">DNI: <input type="text" name="DNI" id="DNI"></label><br>
-    <label for="email">Correo electronico <input type="email" name="email" id="email"></label><br>
-    <label for="imagen">Imagen <input type="file" name="imagen" id="imagen"></label><br>
-    
+    <?php
+
+        $errores = array();
+
+        if(enviado()){
+            if(textoVacio('nombre'))
+                $errores['nombre'] = "El campo nombre está vacío";
+            if(expresionNombre('nombre'))
+                $errores['nombre'] = "El contenido introducido en el nombre no es valido";
+            if(textoVacio('apellido'))
+                $errores['apellido'] = "El campo apellido está vacío";
+            if(expresionApellido('apellido'))
+                $errores['apellido'] = "El contenido introducido en los apellidos no es valido";
+            if(expresionContrasena('contrasena'))
+                $errores['contrasena'] = "El contenido introducido en la contraseña no es valido";
+            if(expresionContrasena('repcontrasena'))
+                $errores['repcontrasena'] = "El contenido introducido en la confirmación de contraseña no es valido";
+            if(coincideContrasena('contrasena','repcontrasena'))
+                $errores['repcontrasena'] = "Las contraseñas no coinciden";
+            if(esMayorEdad('fecha'))
+                $errores['fecha'] = "La fecha es inferior a 18 años";
+            if(expresionDNI('DNI'))
+                $errores['DNI'] = "El formato de DNI es incorrecto";
+            if(expresionCorreo('email'))
+                $errores['email'] = "El formato del correo es incorrecto";
+            if(textoVacio('imagen'))
+                $errores['imagen'] = "El campo imagen está vacio";
+            if(expresionImagen('imagen'))
+                $errores['imagen'] = "El formato de la imagen no es correcto";
+        }
+
+    ?>
+
+    <form action="" method="get" name="formularioPR9" enctype="multipart/form-data">
+        <label for="nombre">Nombre: <input type="text" name="nombre" id="nombre"></label>
+        <?php muestraErrores($errores,'nombre')?><br>
+        <label for="apellido">Apellido: <input type="text" name="apellido" id="apellido"></label>
+        <?php muestraErrores($errores,'apellido')?><br>
+        <label for="contrasena">Contraseña: <input type="password" name="contrasena" id="contrasena"></label>
+        <?php muestraErrores($errores,'contrasena')?><br>
+        <label for="repcontrasena">Confirma contraseña: <input type="password" name="repcontrasena" id="repcontrasena"></label>
+        <?php muestraErrores($errores,'repcontrasena')?><br>
+        <label for="fecha">Fecha <input type="date" name="fecha" id="fecha"></label>
+        <?php muestraErrores($errores,'fecha')?><br>
+        <label for="DNI">DNI: <input type="text" name="DNI" id="DNI"></label>
+        <?php muestraErrores($errores,'DNI')?><br>
+        <label for="email">Correo electronico <input type="text" name="email" id="email"></label>
+        <?php muestraErrores($errores,'email')?><br>
+        <label for="imagen">Imagen <input type="file" name="imagen" id="imagen"></label>
+        <?php muestraErrores($errores,'imagen')?><br>
+        <input type="submit" name="Enviar" id="Enviar">
+    </form>
     
     <?php
         include("../../fragmentos/footer.php");
--- Tema3/PR9/validaciones.php
+++ Tema3/PR9/validaciones.php
@@ -1,2 +1,120 @@
 <?php
+    function enviado(){
+        if(isset($_REQUEST['Enviar']))
+            return true;
+        else
+            return false;
+    }
+
+    function textoVacio($name){
+        if(empty($_REQUEST[$name]))
+            return true;
+        else
+            return false;
+    }
+
+    function muestraErrores($errores,$name){
+        if(isset($errores[$name]))
+            echo $errores[$name];
+    }
+
+    function expresionNombre($name){
+        $exp = '/^[a-zA-Z]{3,}/';
+        $texto = $_REQUEST[$name];
+        if(preg_match($exp,$texto))
+            return false;
+        else
+            return true;
+    }
+
+    function expresionApellido($name){
+        $exp = '/^[a-zA-Z]{3,}\s[a-zA-Z]{3,}/';
+        $texto = $_REQUEST[$name];
+        if(preg_match($exp,$texto))
+            return false;
+        else
+            return true;
+    }
+
+    function expresionContrasena($name){
+        $exp = '/[a-z]{1,}[A-Z]{1,}[0-9]{1,}/';
+        $texto = $_REQUEST[$name];
+        if(preg_match($exp,$texto))
+            return false;
+        else
+            return true;
+    }
+
+    function coincideContrasena($contr,$repcontr){
+        $contrasena = $_REQUEST[$contr];
+        $repContrasena = $_REQUEST[$repcontr];
+
+        if($contrasena === $repContrasena)
+            return false;
+        else
+            return true;
+    }
+
+    function expresionFecha($name){
+
+        $exp = '/(\d{1,2})(\/)(\d{1,2})(\/)(\d{2,4})/';
+        $texto = $_REQUEST[$name];
+
+        if(preg_match($exp,$texto))
+            return false;
+        else
+            return true;
+
+    }
+
+    function esMayorEdad($name){
+        
+        $fechaUsuario = new DateTime($_REQUEST[$name]);
+        $hoy = new DateTime();
+
+        $intervalo = $fechaUsuario->diff($hoy);
+        $intervaloFormateado = $intervalo->format('%y');
+        if($intervaloFormateado >= 18)
+            return false;
+        else
+            return true;
+
+    }
+
+    function expresionDNI($name){
+
+        $exp = '/\d{8}[A-Z]$/';
+        $texto = $_REQUEST[$name];
+
+        if(preg_match($exp,$texto))
+            return false;
+        else
+            return true;
+    }
+
+    function expresionCorreo($name){
+
+        $exp = '/[a-zA-Z0-9]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{1,}/';
+        $texto = $_REQUEST[$name];
+
+        if(preg_match($exp,$texto))
+            return false;
+        else
+            return true;
+    }
+
+    function expresionImagen($name){
+        if(isset($_REQUEST[$name])){
+            $exp = '/^[a-zA-Z0-9]+\.(jpg|png|bmp)$/';
+            $texto = $_REQUEST[$name];
+
+            if(preg_match($exp,$texto))
+                return false;
+            else
+                return true;
+        }else{
+            return true;
+        }
+    }
+
 ?>
