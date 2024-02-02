7f96dea6bea905d05379c2c045676f8db83ef15e
Fernando Calles <fercara1999@gmail.com>
2024-02-02 10:33:20 +0000

    2F

--- Tema7/apiCine/consumirAPI/modificar.php
+++ Tema7/apiCine/consumirAPI/modificar.php
@@ -8,7 +8,7 @@ require("configurarAPI.php");
             $array['titulo'] = $_REQUEST['titulo'];
         }
         if(!empty($_REQUEST['directo'])){
-            $array['director'] = $_REQUEST['director'];
+            $array['director'] = $_REQUEST['directo'];
         }
         if(!empty($_REQUEST['genero'])){
             $array['genero'] = $_REQUEST['genero'];
@@ -26,5 +26,5 @@ require("configurarAPI.php");
     <label for="titulo">Titulo: <input type="text" name="titulo" id="titulo"></label>
     <label for="directo">Director: <input type="text" name="directo" id="directo"></label><br>
     <label for="genero">Genero: <input type="text" name="genero" id="genero"></label><br>
-    <input type="submit" name="insertar" id="insertar">
+    <input type="submit" name="modificar" id="modificar">
 </form>
